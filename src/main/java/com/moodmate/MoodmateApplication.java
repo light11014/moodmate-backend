1e98bdbf384ebebae450d6ac91274c90407a468b
light11014 <dtq1014@naver.com>
2025-05-07 10:11:14 +0900

    Initial commit

---
+++ src/main/java/com/moodmate/MoodmateApplication.java
@@ -0,0 +1,13 @@
+package com.moodmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moodmate/MoodmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
